26dfde6dbd8b4511eef4345f8e28415a7d1ad7c7
Jordi Vilaplana <gatasuna@gmail.com>
2014-07-09 12:13:35 +0200

    Tests & added the scheduler file

--- greenc.py
+++ greenc.py
@@ -14,6 +14,28 @@
 
 from novaclient.v1_1 import client
 
-nt = client.Client("jvilaplana", "mypassword", "bigproject", "http://stormy.udl.net:5000/v2.0/", service_type="compute")
+nova = client.Client("jvilaplana", "mypassword", "bigproject", "http://stormy.udl.net:5000/v2.0/", service_type="compute")
 
-print nt.flavors.list()
+print nova.servers.list()
+
+print nova.flavors.list()
+
+# List all the availability zones
+av_zones = nova.availability_zones.list()
+
+# List that will contain all the availability zones used by GreenC
+greenc_zones = []
+
+for az in range(0, len(av_zones)):
+    if av_zones[az].zoneName.startswith("greenc_"):
+        greenc_zones.append(av_zones[az])
+
+
+print av_zones[1].hosts
+
+    #print av_zones[az]
+    #print dir(av_zones[az])
+    #print av_zones[az].zoneName
+
+
+#printnova.servers.create("my-server", flavor=fl)
